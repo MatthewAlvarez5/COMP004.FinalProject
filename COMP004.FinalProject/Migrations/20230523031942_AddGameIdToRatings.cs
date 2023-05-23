447b8265b6830d8bbab3f03abcf5501d997a8324
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-05-22 20:24:27 -0700

    HOpefully nothing wrong

---
+++ COMP004.FinalProject/Migrations/20230523031942_AddGameIdToRatings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP004.FinalProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameIdToRatings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
